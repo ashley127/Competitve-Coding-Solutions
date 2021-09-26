95ae64549cbdd1707b431e33272d7f5e939b6af7
Ashley Zhao <74732255+ashley127@users.noreply.github.com>
2021-09-26 16:11:27 -0400

    Update UnnecessaryTrashPush.java

--- DMOJ/UnnecessaryTrashPush.java
+++ DMOJ/UnnecessaryTrashPush.java
@@ -1,3 +1,20 @@
+/*
+Brandon likes to detonate fireworks. However, fireworks leave behind a lot of trash. 
+Brandon has adjusted the Trash Push to clear out arbitrary trash, but the Trash Push is extremely tiring to perform after every firework detonation. 
+Brandon wonders if some of his Trash Pushes are unnecessary.
+
+We will model how Brandon takes out the trash as follows - he has infinitely many trash cans that can store up to K units of trash. Over the course of N
+days, Brandon will detonate some fireworks. On day i, Brandon will accumulate ai units of trash. When Brandon executes a Trash Push, he takes all the
+trash he has and pushes it into a trash can. As before, the trash that overflows will automatically enter another trash can. A Trash Push is unnecessary 
+if, after executing it, no trash can is completely full of trash. Brandon can execute a Trash Push at any time, as long as he has at least one unit of 
+trash to push. After executing a Trash Push, Brandon takes all trash cans that have trash in them and takes them outside. They are emptied overnight and
+Brandon brings them back in the next day.
+
+Given the amount of trash Brandon will generate over the next N days, compute the maximum number of Trash Pushes Brandon can perform, given that
+none of them are unnecessary.
+*/
+
+
 package DMOJ;
 import java.util.*;
 public class UnnecessaryTrashPush
