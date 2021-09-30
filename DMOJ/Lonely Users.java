7c4458f198bbb982a6b8bd8ded7919fe5600dfb7
Ashley Zhao <74732255+ashley127@users.noreply.github.com>
2021-09-30 18:05:09 -0400

    Create Lonely Users.java

---
+++ DMOJ/Lonely Users.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    public static void main(String[] args) throws IOException {
+        int testCase = readInt();
+        for(int i = 0; i<testCase; i++){
+            int users = readInt();
+            if(users ==2){
+                System.out.println("2");
+                continue;
+            }
+            System.out.println(users-1);
+        }
+    }
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens())
+            st = new StringTokenizer(br.readLine().trim());
+        return st.nextToken();
+    }
+
+    static long readLong() throws IOException {
+        return Long.parseLong(next());
+    }
+
+    static int readInt() throws IOException {
+        return Integer.parseInt(next());
+    }
+
+    static double readDouble() throws IOException {
+        return Double.parseDouble(next());
+    }
+
+    static char readCharacter() throws IOException {
+        return next().charAt(0);
+    }
+
+    static String readLine() throws IOException {
+        return br.readLine().trim(); 
+    }
+}
