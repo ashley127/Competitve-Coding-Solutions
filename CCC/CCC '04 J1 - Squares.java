85dfe7a9ac1197d4f04279ac116e44c85af1b33f
Ashley Zhao <74732255+ashley127@users.noreply.github.com>
2021-10-11 20:19:18 -0400

    Create CCC '04 J1 - Squares.java

---
+++ CCC/CCC '04 J1 - Squares.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int result = (int)Math.sqrt(num);
+        System.out.println("The largest square has side length " + result+ ".");
+    }
+}
